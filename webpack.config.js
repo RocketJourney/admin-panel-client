a50b0c594404b05320b66b64607346e4adf78610
Gustavo <gmora008@gmail.com>
2018-06-08 18:27:39 -0500

    add config and button

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var HtmlWebPackPlugin = require("html-webpack-plugin");
-var path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
@@ -10,14 +10,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
       },
       {
-        test: /\.less$/,
+        test: /\.less?$/,
         use: [
           {
             loader: "style-loader"
@@ -31,11 +31,31 @@ module.exports = {
             }
           },
           {
-            loader: "less-loader"
+            loader: "less-loader",
+            options: {
+              paths: [
+                path.resolve(__dirname, "node_modules"),
+                path.resolve(__dirname, "src")
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name]-[hash:8].[ext]"
+            }
           }
         ]
       }
     ]
+  },
+  resolve: {
+    extensions: [".less", ".js", ".jsx", ".react.js"]
   },
   plugins: [htmlPlugin]
 };
